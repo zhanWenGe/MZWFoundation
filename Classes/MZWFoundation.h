df9073944c6a6d90aabd4deaac2b7825898b44d9
zhanWenGe <403853144@qq.com>
2017-11-08 14:07:04 +0800

    修改框架名称

--- Classes/MZWFoundation.h
+++ Classes/MZWFoundation.h
@@ -6,14 +6,14 @@
 //  Copyright © 2017年 PROJECT_OWNER. All rights reserved.
 //
 
-#ifndef ZWFoundation_h
-#define ZWFoundation_h
+#ifndef MZWFoundation_h
+#define MZWFoundation_h
 
-#if __has_include(<ZWFundation/ZWFundation.h>)
-#import <ZWFundation/NSDate+Expand.h>
-#import <ZWFundation/NSDictionary+Expand.h>
-#import <ZWFundation/NSError+HTTPS.h>
-#import <ZWFundation/NSString+Expand.h>
+#if __has_include(<MZWFundation/MZWFundation.h>)
+#import <MZWFundation/NSDate+Expand.h>
+#import <MZWFundation/NSDictionary+Expand.h>
+#import <MZWFundation/NSError+HTTPS.h>
+#import <MZWFundation/NSString+Expand.h>
 #else
 #import "NSDate+Expand.h"
 #import "NSDictionary+Expand.h"
